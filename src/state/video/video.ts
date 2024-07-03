b883fc582e4d07ffcdcc9bda86e7f979060881e8
João Alberto <hey@joaoalberto.dev>
2024-07-03 20:26:00 -0300

    chore(video): adding settings to video store

--- src/state/video/video.ts
+++ src/state/video/video.ts
@@ -1,13 +1,23 @@
 import { create } from "zustand";
 
+type VideoSettings = {
+  width: number;
+  height: number;
+  aspect: number;
+  duration: number;
+};
+
 type VideoStore = {
   video: File | null;
+  settings: VideoSettings | null;
   setVideo: (video: File) => void;
   clearVideo: () => void;
+  setSettings: (settings: VideoSettings) => void;
 };
 
 const useVideoStore = create<VideoStore>((set) => ({
   video: null,
+  settings: null,
   setVideo: (video: File) => {
     if (video instanceof File === false) {
       throw new Error("Param 'video' is not a valid File");
@@ -15,7 +25,8 @@ const useVideoStore = create<VideoStore>((set) => ({
 
     return set({ video });
   },
-  clearVideo: () => set({ video: null }),
+  clearVideo: () => set({ video: null, settings: null }),
+  setSettings: (settings: VideoStore["settings"]) => set({ settings }),
 }));
 
 export { useVideoStore };
