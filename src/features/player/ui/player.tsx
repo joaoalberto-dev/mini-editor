7b3971545090cd69298f17c18242082505fefd62
João Alberto <hey@joaoalberto.dev>
2024-07-03 17:55:19 -0300

    chore(player): change player to receive children

--- src/features/player/ui/player.tsx
+++ src/features/player/ui/player.tsx
@@ -1,5 +1,13 @@
-function Player() {
-  return <div className="min-h-[600px] border-b-2 bg-neutral-200 flex items-center justify-center">Player</div>;
+import type { WithChildren } from "@/types/with-children";
+
+type PlayerProps = WithChildren;
+
+function Player({ children }: PlayerProps) {
+  return (
+    <div className="min-h-[600px] border-b-2 bg-neutral-200 flex items-center justify-center">
+      {children}
+    </div>
+  );
 }
 
-export { Player }
+export { Player };
