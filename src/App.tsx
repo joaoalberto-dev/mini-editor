b94cff2511764b774731dda10ec9ff58cb452183
João Alberto <hey@joaoalberto.dev>
2024-07-03 17:55:20 -0300

    chore(app): render upload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { AppContainer } from "@/ui/app-container";
 import { Player } from "@/features/player/ui/player";
 import { Timeline } from "@/features/timeline/ui/timeline";
+import { Upload } from "@/features/upload/ui/upload";
+import { useVideoStore } from "./state/video/video";
 
 function App() {
+  const { video } = useVideoStore();
+
   return (
     <AppContainer>
-      <Player />
+      <Player>{video ? null : <Upload />}</Player>
       <Timeline />
     </AppContainer>
   );
