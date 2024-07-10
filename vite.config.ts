e04f3a0b44f1b4ce2999f6b65d885fabdae1b0b4
João Alberto <hey@joaoalberto.dev>
2024-07-10 02:34:26 -0300

    chore: alias new editor state

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@state/editor": path.resolve("./src/state/editor"),
     },
   },
   server: {
