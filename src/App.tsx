76a8b5031224f44fa7587d4efc08380ae5b61f16
João Alberto <joao.oliveira@fluencyuacademy.io>
2024-07-10 01:03:26 -0300

    chore: stop rendering playback, controls and timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { AppContainer } from "@/ui/app-container";
 import { Player } from "@/features/player/ui/player";
-import { Timeline } from "@/features/timeline/ui/timeline";
 import { Upload } from "@/features/upload/ui/upload";
-import { useVideoStore } from "@/state/video/video";
-import { Playback } from "@/features/playback/ui/playback";
-import { PlaybackControls } from "@/features/playback-controls/ui/playback-controls";
 
 function App() {
-  const { videoSource } = useVideoStore();
+
 
   return (
     <AppContainer>
       <Player>
-        {videoSource ? <Playback src={videoSource} /> : <Upload />}
+        <Upload />
       </Player>
-      <PlaybackControls />
-      <Timeline />
     </AppContainer>
   );
 }
